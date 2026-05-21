058569c7a739d73ee06b65308e302f832b22214f
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-05-21 20:49:51 +0530

    smart-career-copilot

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -6,9 +6,9 @@ export const AuthProvider =({children}) => {
     const [user,setUser] = useState(null)
     const [loading,setLoading] = useState(false)
 
-    return {
-        <AuthContext.Provider value = {{user,setUser,loading,setLoading}}>
+    return (
+        <AuthContext.Provider value = {{user,setUser,loading,setLoading}} >
         {children}
         </AuthContext.Provider>
-    }
+    )
 }
