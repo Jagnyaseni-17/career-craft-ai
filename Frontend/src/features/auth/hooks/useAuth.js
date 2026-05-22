78e98d96a2973ee45815bb020ee650e163537a5d
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-05-22 22:54:42 +0530

    smart-career-copilot

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -9,23 +9,38 @@ export const useAuth = () => {
 
     const handleLogin = async ({email, password}) => {
         setLoading(true)
-        const data = await login({ email, password })
-        setUser(data.user)
+        try{
+            const data = await login({email, password})
+            setUser(data.user)
+        }catch(err){
+
+        }finally{
         setLoading(false)
+        }
     }
 
     const handleRegister = async ({username, email, password}) => {
         setLoading(true)
+        try{
         const data = await register({ username, email, password })
         setUser(data.user)
+        }catch(err){
+
+        }finally{
         setLoading(false)
+        }
     }
 
     const handleLogout = async () => {
         setLoading(true)
+        try{
         const data = await logout()
         setUser(null)
+        }catch(err){
+
+        }finally{
         setLoading(false)
+        }
     }
 
     return {user,loading,handleRegister,handleLogin,handleLogout}
