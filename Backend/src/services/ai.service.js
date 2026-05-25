31a120ef012d8f21f78d64f0ccfb2e41b2ad39f8
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-05-25 19:50:28 +0530

    smart-career-copilot

---
+++ Backend/src/services/ai.service.js
@@ -0,0 +1,53 @@
+const {GoogleGenAI} = require("@google/genai")
+const {z} = require("zod")
+const {zodToJsonSchema} = require("zod-to-json-schema")
+
+const ai = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_GENAI_API_KEY
+})
+
+const interviewReportSchema = z.object({
+    technicalQuestions: z.array(z.object({
+        question: z.string().description("The technical question can be asked in the interview"),
+        intension: z.string().description("The intension of interviewer behind asking this question"),
+        answer: z.string().description("How to answer this question, what points to cover,what approach to take etc.")
+    })).description("Technical questions that can be asked in the interview along with their intention and how to answer them"),
+    behaviouralQuestions:  z.array(z.object({
+        question: z.string().description("The technical question can be asked in the interview"),
+        intension: z.string().description("The intension of interviewer behind asking this question"),
+        answer: z.string().description("How to answer this question, what points to cover,what approach to take etc.")
+    })).description("Behavioural questions that can be asked in the interview along with their intention and how to answer them"),
+    skillGaps: z.array(z.object({
+        skill: z.string().description("The skill which the candidate is lacking"),
+        severity: z.enum(["low","medium","high"]).description("The severity of this skill gap, i.e. how important is this skill for the job and how much it can impact the candidate's chances")
+    })).describe("List of skill gaps in the candidate's profile along with their severity"),
+    preparationPlan: z.array(z.object({
+        day: z.number().describe("The day number in the preparation plan, starting from 1"),
+        focus: z.string().describe("The main focus of this day in the preparation plan, e.g. data structures, system design, mock interviews etc."),
+        tasks: z.array(z.string()).describe("List of tasks to be done on this day to follow the preparation plan, e.g. read a specific book or article, solve a set of problems, watch a video etc.")
+    })).describe("A day-wise preparation plan for the candidate to follow in order to prepare for the interview effectively"),
+    title: z.string().describe("The title of the job for which the interview report is generated"),
+})
+
+async function generateInterviewReport({ resume, selfDescription, jobDescription }) {
+
+
+    const prompt = `Generate an interview report for a candidate with the following details:
+                        Resume: ${resume}
+                        Self Description: ${selfDescription}
+                        Job Description: ${jobDescription}
+`
+
+    const response = await ai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: prompt,
+        config: {
+            responseMimeType: "application/json",
+            responseSchema: zodToJsonSchema(interviewReportSchema),
+        }
+    })
+
+    return JSON.parse(response.text)
+
+
+}
